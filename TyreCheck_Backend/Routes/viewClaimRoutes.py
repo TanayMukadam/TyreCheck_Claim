927536a86c0494a94fad03bb2b896630f2db8b95
Tanay <tanaymukadam@gmail.com>
2025-12-10 11:39:43 +0530

    View Claim Route

--- TyreCheck_Backend/Routes/viewClaimRoutes.py
+++ TyreCheck_Backend/Routes/viewClaimRoutes.py
@@ -1,6 +1,7 @@
 from Database.database import get_db
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from Utils.auth import get_current_user
 import logging
 
@@ -13,7 +14,36 @@ protected_claimView_route = APIRouter(
     dependencies=[Depends(get_current_user)]
 )
 
+# #claim_id = ROH2/25/045866
+# @protected_claimView_route.get("/Claim_ID={claim_id}")
+# async def claimViewRoute(claim_id: str, db: Session = Depends(get_db)):
+    
+#     if not claim_id:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"message": "Claim ID Record not found"})
+    
+#     sql = text("CALL tyrecheck.USP_GetTyreDetailsFromWarranty_ClaimNo(:claim_id)")
+#     result = db.execute(sql, {"claim_id": claim_id})
+#     data = result.fetchall()
+    
+#     columns = result.keys()
+#     response = [dict(zip(columns, row)) for row in data]
+    
+#     return response
 
-@protected_claimView_route.get("")
-async def claimViewRoute(db: Session = Depends(get_db)):
-    return "hello World"
+@protected_claimView_route.get("/Claim_ID={claim_id:path}")
+async def claimViewRoute(claim_id: str, db: Session = Depends(get_db)):
+    
+    if not claim_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": "Claim ID Record not found"}
+        )
+    
+    sql = text("CALL tyrecheck.USP_GetTyreDetailsFromWarranty_ClaimNo(:claim_id)")
+    result = db.execute(sql, {"claim_id": claim_id})
+    data = result.fetchall()
+    
+    columns = result.keys()
+    response = [dict(zip(columns, row)) for row in data]
+    
+    return response
