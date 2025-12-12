6ac5104cf79098c262d1a5db996698920bf9b76e
Tanay <tanaymukadam@gmail.com>
2025-12-12 11:01:38 +0530

    Docker File Update

--- TyreCheck_Frontend/src/constants/tyrecheck.constants.js
+++ TyreCheck_Frontend/src/constants/tyrecheck.constants.js
@@ -1,7 +1,8 @@
 
 
 
+// const tyrecheck_api_url = "http://tyreclaimbackend.tyrechecks.com:8086"
 const tyrecheck_api_url = "http://127.0.0.1:8000"
 
 
-export default tyrecheck_api_url
+export default tyrecheck_api_url; 
